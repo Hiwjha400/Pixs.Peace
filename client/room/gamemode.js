903b52f9f1438cb0cb79ccc0ea2e49e223c174fa
Hiwjha400 <024aaz17@gmail.com>
2026-02-26 18:24:12 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -2,12 +2,3 @@ import * as peace from './peace_library.js';
 
 peace.configure();
 peace.create_teams();
-
-const ADMINS = [
-    "6A2E3EB992DD73E4",
-    "10232242",
-    "35834829"
-];
-
-function isAdmin(player) {
-    return ADMINS.includes(player.id);
