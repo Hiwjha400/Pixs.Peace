d743113a39aac5ae4f4540ac9a54b632476af27e
Hiwjha400 <024aaz17@gmail.com>
2026-02-26 15:21:48 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -2,3 +2,25 @@ import * as peace from './peace_library.js';
 
 peace.configure();
 peace.create_teams();
+
+}
+
+import * as peace from './peace.library.js';
+
+peace.createRoom({
+    // настройки режима
+});
+
+// 👇 СЮДА
+
+peace.on("playerSpawn", (player) => {
+
+    if (player.team === BLUE_TEAM) {
+        player.setSkin("prisoner");
+    }
+
+    if (player.team === RED_TEAM) {
+        player.setSkin("prisoner");
+    }
+
+});
